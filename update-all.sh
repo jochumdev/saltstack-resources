42f81ec4d4a0d4abef65cb20be6a7e8ab7232ee5
René Jochum <rene@jochums.at>
2015-05-19 19:18:51 +0200

    Extended update-all.sh with submodule updates and updates them all.

--- update-all.sh
+++ update-all.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
 # http://stackoverflow.com/a/19029685
 
+oldPWD=$(pwd)
+
+cd formulas/saltstack-formulas
+curl -s https://api.github.com/orgs/saltstack-formulas/repos?per_page=200 | ruby -rubygems -e '
+  require "json";
+  JSON.load(STDIN.read).each {
+    |repo|
+    if !File.directory?(repo["name"])
+      print "git submodule add #{repo["clone_url"]} #{repo["name"]} "
+      %x[ git submodule add #{repo["clone_url"]} #{repo["name"]} ]
+    end
+  }
+'
+
+cd $oldPWD
+cd formulas/bechtoldt
+curl -s https://api.github.com/orgs/bechtoldt/repos?per_page=200 | ruby -rubygems -e '
+  require "json";
+  JSON.load(STDIN.read).each {
+    |repo|
+    if !File.directory?(repo["name"])
+      print "git submodule add #{repo["clone_url"]} #{repo["name"]} "
+      %x[ git submodule add #{repo["clone_url"]} #{repo["name"]} ]
+    end
+  }
+'
+
 git submodule update --init --recursive
 git submodule foreach --recursive git fetch
 git submodule foreach git pull --ff-only origin master
